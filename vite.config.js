f06e7b0342815ca523f7ef17408cb37d3f36111b
subhasmita sahoo <subhasmita4602@gmail.com>
2025-02-04 17:26:05 +0530

    add file

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
-]
+],
+build: {
+  outDir: 'dist',
+  // Ensure proper asset handling 
+  assetsDir: 'assets',
+  //Generate proper asset names
+  rollupOptions: {
+    output:{
+      assetFileNames: 'assets/[name]-[hash][extname]',
+      chunkFileNames: 'assets/[name]-[hash].js',
+      entryFileNames: 'assets/[name]-[hash].js',
+    }
+  }
+}
 })
